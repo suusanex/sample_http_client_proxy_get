ddeff7fb12fbb3202a41346d19f567b08c94c00e
suusanex <suusan@himado.net>
2020-05-10 15:39:31 +0900

    プロキシ情報の取得について実験が終わった時点のソースコードと、実験結果の簡単なメモ（ReadMe）。

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # sample_http_client_proxy_get
+
+通常設定では、プロキシの設定情報はHKCUに書かれる。（`HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings`）
+
+ProxySettingsPerUserを設定してプロキシをPC単位にしてある場合、HKCUの同じキーに書かれる。
+
+どちらのケースでも、この実装でプロキシを取得することができた。PC単位にしてある場合に限り、Windowsサービス（LocalSystem）からもプロキシ情報が取得できる。
+
+※この実装は.NET Core 3.1では動作しないことがすでに分かっている（WebProxyが実装に使われていないため）
---
+++ sample_http_client_proxy/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace sample_http_client_proxy
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnMainBtn(object sender, RoutedEventArgs e)
+        {
+            var outBuf = new StringBuilder();
+
+            var proxy = WebRequest.GetSystemWebProxy();
+
+            WebProxy webProxy = proxy as WebProxy;
+            if (webProxy == null)
+            {
+                //.NET 4.7.2実装依存
+                var field = proxy.GetType().GetField("webProxy",
+                    BindingFlags.GetField | BindingFlags.Instance | BindingFlags.NonPublic);
+                if (field != null)
+                {
+                    webProxy = (WebProxy)field.GetValue(proxy);
+                }
+            }
+
+            if (webProxy == null)
+            {
+                return;
+            }
+
+            outBuf.AppendLine($"Address={webProxy.Address}");
+
+            IFormatter formatter = new BinaryFormatter();
+            using (var stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, webProxy);
+
+                var data = stream.ToArray();
+
+                using (var stream2 = new MemoryStream(data))
+                {
+                    var proxy2 = (WebProxy)formatter.Deserialize(stream2);
+
+                    outBuf.AppendLine($"Address={proxy2.Address}");
+
+                }
+
+            }
+
+            MessageBox.Show(outBuf.ToString());
+        }
+    }
+}
