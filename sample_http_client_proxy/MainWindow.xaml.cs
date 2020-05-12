3871e7835f2b0764122efe8f0705306ee115ee10
suusanex <suusan@himado.net>
2020-05-13 08:58:13 +0900

    実際に通信する実験を行い、元の方式ではWPADやPACに対応できなかったことをコメント記載。特定のURLと通信する際のプロキシURLを取得しておき、それを再利用する方式を追加で実験。こちらはWPAD・PACも含めて成功。URLごとの例外がある場合には対応できないが、ある程度実用的な方法と言える。

--- sample_http_client_proxy/MainWindow.xaml.cs
+++ sample_http_client_proxy/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Reflection;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
@@ -35,6 +36,38 @@ namespace sample_http_client_proxy
         {
             var outBuf = new StringBuilder();
 
+            var proxy = ProxyGetTest(outBuf);
+
+            MessageBox.Show(outBuf.ToString());
+
+
+            var cli = WebRequest.Create(m_TestUri);
+            cli.Proxy = proxy;
+            cli.Method = "GET";
+            using (var res = cli.GetResponse())
+            using (var resStream = new StreamReader(res.GetResponseStream()))
+            {
+                MessageBox.Show($"{resStream.ReadToEnd().Substring(0, 300)}");
+            }
+        }
+
+        static IWebProxy ProxyGetTest(StringBuilder outBuf)
+        {
+            return ProxyGet(outBuf);
+        }
+        static IWebProxy ProxyGet(StringBuilder outBuf)
+        {
+
+            var proxy = WebRequest.GetSystemWebProxy();
+            outBuf.AppendLine($"Address={proxy.GetProxy(new Uri(m_TestUri))}");
+            return proxy;
+        }
+
+
+        private static WebProxy ProxyGetAndSerialize(StringBuilder outBuf)
+        {
+            //この関数のようにバイナリシリアライズする方式では、プロキシのアドレスが直接指定されている場合は設定を保存できるが、WPADの自動検出やPACファイルでの指定の場合は設定を保存できなかった。
+
             var proxy = WebRequest.GetSystemWebProxy();
 
             WebProxy webProxy = proxy as WebProxy;
@@ -45,13 +78,13 @@ namespace sample_http_client_proxy
                     BindingFlags.GetField | BindingFlags.Instance | BindingFlags.NonPublic);
                 if (field != null)
                 {
-                    webProxy = (WebProxy)field.GetValue(proxy);
+                    webProxy = (WebProxy) field.GetValue(proxy);
                 }
             }
 
             if (webProxy == null)
             {
-                return;
+                return null;
             }
 
             outBuf.AppendLine($"Address={webProxy.Address}");
@@ -65,15 +98,99 @@ namespace sample_http_client_proxy
 
                 using (var stream2 = new MemoryStream(data))
                 {
-                    var proxy2 = (WebProxy)formatter.Deserialize(stream2);
+                    var proxy2 = (WebProxy) formatter.Deserialize(stream2);
 
                     outBuf.AppendLine($"Address={proxy2.Address}");
 
+                    return proxy2;
                 }
-
             }
 
+        }
+
+        private async void OnOldTypeBtn(object sender, RoutedEventArgs e)
+        {
+
+            var outBuf = new StringBuilder();
+
+            var proxy = ProxyGetTest(outBuf);
+
             MessageBox.Show(outBuf.ToString());
+
+            var handler = new HttpClientHandler()
+            {
+                Proxy = proxy,
+            };
+
+            using (var cli = new HttpClient(handler))
+            {
+
+                using (var res = await cli.GetAsync(m_TestUri))
+                {
+                    MessageBox.Show($"{(await res.Content.ReadAsStringAsync()).Substring(0, 300)}");
+                }
+            }
+        }
+
+        private static string m_TestUri = "https://www.google.co.jp/";
+
+        private void OnMainBtnDirectProxy(object sender, RoutedEventArgs e)
+        {
+            var outBuf = new StringBuilder();
+
+            var proxy = ProxyGetTest(outBuf);
+
+            MessageBox.Show(outBuf.ToString());
+
+
+            var cli = WebRequest.Create(m_TestUri);
+            if (proxy.IsBypassed(new Uri(m_TestUri)))
+            {
+                cli.Proxy = null;
+            }
+            else
+            {
+                cli.Proxy = new WebProxy(proxy.GetProxy(new Uri(m_TestUri)));
+            }
+
+            cli.Method = "GET";
+            using (var res = cli.GetResponse())
+            using (var resStream = new StreamReader(res.GetResponseStream()))
+            {
+                MessageBox.Show($"{resStream.ReadToEnd().Substring(0, 300)}");
+            }
+            
+        }
+
+        private async void OnOldTypeBtnDirectProxy(object sender, RoutedEventArgs e)
+        {
+
+            var outBuf = new StringBuilder();
+
+            var proxy = ProxyGetTest(outBuf);
+
+            MessageBox.Show(outBuf.ToString());
+
+            var handler = new HttpClientHandler();
+
+            if (proxy.IsBypassed(new Uri(m_TestUri)))
+            {
+                handler.Proxy = null;
+                handler.UseProxy = false;
+            }
+            else
+            {
+                handler.Proxy = new WebProxy(proxy.GetProxy(new Uri(m_TestUri)));
+                handler.UseProxy = true;
+            }
+
+            using (var cli = new HttpClient(handler))
+            {
+                using (var res = await cli.GetAsync(m_TestUri))
+                {
+                    MessageBox.Show($"{(await res.Content.ReadAsStringAsync()).Substring(0, 300)}");
+                }
+            }
         }
     }
 }
